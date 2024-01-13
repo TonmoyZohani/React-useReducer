2364d2c9fa0276e8c2618e60c654ed7403a443d2
TonmoyZohani <tonmoyzohani@gmail.com>
2024-01-13 22:38:16 +0600

    questions saved

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
-import { useEffect } from "react";
+import { useEffect, useReducer } from "react";
 import Header from "./Header.js";
 import Main from "./Main.js";
 
+const initialState = {
+  questions: [],
+  status: "loading",
+};
+
+const reducer = (state, action) => {
+  switch ((action.type)) {
+    case "dataReceived":
+      return { ...state, questions: action.payload };
+  }
+};
+
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
   useEffect(() => {
     fetch(`http://localhost:8000/questions`)
       .then((res) => res.json())
-      .then((data) => console.log(data));
+      .then((data) => dispatch({ type: "dataReceived", payload: data }));
   }, []);
+
+  console.log(state);
 
   return (
     <div className="app">
