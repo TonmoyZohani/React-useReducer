a6484d4dca679a4ec68b2a29f87c19b0955c686b
TonmoyZohani <tonmoyzohani@gmail.com>
2024-01-13 19:35:24 +0600

    reset done by payload

--- src/DateCounter.js
+++ src/DateCounter.js
@@ -4,6 +4,10 @@ const reducer = (state, action) => {
   switch (action.type) {
     case "inc":
       return { ...state, count: state.count + 1 };
+    case "dec":
+      return { ...state, count: state.count - 1 };
+    case 'res':
+      return {...state,count: action.payload};
   }
 };
 
