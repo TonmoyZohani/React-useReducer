333fb6e509737371aa9ebb5c0b7cead11f6fc372
TonmoyZohani <tonmoyzohani@gmail.com>
2024-01-13 22:19:11 +0600

    fetch the questions

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
+import { useEffect } from "react";
 import Header from "./Header.js";
-import Main from './Main.js';
+import Main from "./Main.js";
 
 function App() {
+  useEffect(() => {
+    fetch(`http://localhost:8000/questions`)
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  }, []);
+
   return (
     <div className="app">
       <Header />
