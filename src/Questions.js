5d35b3d5f13ac4e9d688f166ec6a36cd19fe9ea2
TonmoyZohani <tonmoyzohani@gmail.com>
2024-01-14 22:37:48 +0600

    answer selected

--- src/Questions.js
+++ src/Questions.js
@@ -3,15 +3,24 @@ import React from "react";
 const Questions = ({ questions, dispatch, answer }) => {
   console.log(questions);
 
+  const hasAnswer = answer !== null;
+
   return (
     <div>
       <h4>{questions?.question}</h4>
       <div className="options">
         {questions?.options.map((option, index) => (
           <button
-            className={`btn btn-option ${index === answer ? "answer" : ""}`}
+            className={`btn btn-option ${index === answer ? "answer" : ""} ${
+              hasAnswer
+                ? index === questions.correctOption
+                  ? "correct"
+                  : "wrong"
+                : ""
+            }`}
             key={option}
             onClick={() => dispatch({ type: "newAnswer", payload: index })}
+            disabled={hasAnswer}
           >
             {option}
           </button>
