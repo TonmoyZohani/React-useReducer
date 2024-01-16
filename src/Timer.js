f9413665fadcb82611eb897522f61e93ba15c2b1
TonmoyZohani <tonmoyzohani@gmail.com>
2024-01-16 23:26:42 +0600

    clear interval added

--- src/Timer.js
+++ src/Timer.js
@@ -2,9 +2,11 @@ import React, { useEffect } from "react";
 
 const Timer = ({ secondsRemain, dispatch }) => {
   useEffect(() => {
-    setInterval(() => {
+    const id = setInterval(() => {
       dispatch({ type: "tick" });
     }, 1000);
+
+    return () => clearInterval(id)
   }, [dispatch]);
 
   return <div>{secondsRemain}</div>;
